05a0e0122a08ce03195e38d60e2fe6dafa0380e5
KuvvollaMurali123 <kuvvollamurali2@gmail.com>
2024-02-21 12:03:25 +0530

    testcases

---
+++ CabInvoiceTests1/CabInvoiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class CabInvoiceTests
+    {
+        [TestMethod()]
+        public void TotalFareTest()
+        {
+            double distance = 5;
+            int Time = 2;
+            int Expectred = 70;
+            double Actual = (CabInvoice.TotalFare(5,2));
+            Assert.AreEqual(Expectred,Actual);
+
+        }
+    }
+}
