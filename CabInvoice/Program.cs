fba8df09264b65a11c44f3e9eeadb1fc806d69cc
raghu <subamraghu11154@gmail.com>
2024-02-21 11:25:59 +0530

    first commit

---
+++ CabInvoice/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("hii");
+        Console.WriteLine(CabInvoice.TotalFare(5,2));
+    }
+}
+public class CabInvoice
+{
+    static double costPerKM=10;
+    static double costPerMin=10;
+    static double MinFare=5;
+    public static double TotalFare(double distance,int time)
+    {
+        double totval=(distance*costPerKM)+(time*costPerMin);
+        return Math.Max(totval,MinFare);
+    }
+}
